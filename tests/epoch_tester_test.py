ac1001819c96da8302814544eaf5e113a615252c
Martin van Wingerden <martin.van.wingerden@diginh.nl>
2015-07-15 14:50:14 +0200

    Update the scores to be compatible with the new dynamic scoring

--- tests/epoch_tester_test.py
+++ tests/epoch_tester_test.py
@@ -12,27 +12,27 @@ class EpochTesterTestTest(unittest.TestCase):
         value_numeric = 1164287633
         value = self.tester.test(value_numeric)
         self.assertEqual(len(value),  1)
-        self.assertEqual(value.get('FAT timestamp'), 1.0)
+        self.assertEqual(value.get('FAT timestamp'), 0.9)
 
 
     def test_single_4byte(self):
         value_numeric = 2999663906
         value = self.tester.test(value_numeric)
         self.assertEqual(len(value),  1)
-        self.assertEqual(value.get('4-Bytes bit-based timestamp since 1970'), 1.0)
+        self.assertEqual(value.get('4-Bytes bit-based timestamp since 1970'), 0.9)
 
 
     def test_single_5byte(self):
         value_numeric = 767981813774
         value = self.tester.test(value_numeric)
         self.assertEqual(len(value),  1)
-        self.assertEqual(value.get('5-Bytes bit-based timestamp'), 1.0)
+        self.assertEqual(value.get('5-Bytes bit-based timestamp'), 0.9)
 
 
     def test_multi(self):
         values_numeric = (1164287633,1164287633,1164287633, 2999663906, 767981813774,)
         value = self.tester.test(values_numeric)
         self.assertEqual(len(value),  3)
-        self.assertEqual(value.get('FAT timestamp'), 0.6)
-        self.assertEqual(value.get('4-Bytes bit-based timestamp since 1970'), 0.2)
-        self.assertEqual(value.get('5-Bytes bit-based timestamp'), 0.2)
+        self.assertEqual(value.get('FAT timestamp'), 0.54)
+        self.assertEqual(value.get('4-Bytes bit-based timestamp since 1970'), 0.18)
+        self.assertEqual(value.get('5-Bytes bit-based timestamp'), 0.18)
